5fbdb7bc2f655cdcaf52f09322464ec2de2b2527
isaeme23 <isabella.manrique@mail.escuelaing.edu.co>
2023-09-13 15:26:44 -0500

    A bit fixed

--- src/main/java/com/example/postgreslab/Postgres/repository/UserRepository.java
+++ src/main/java/com/example/postgreslab/Postgres/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.postgreslab.Postgres.repository;
 
+import com.example.postgreslab.Postgres.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
